e0c0f7cdde199fe871f5c5dad6e622ea6ca99509
Anton <oxlamons@gmail.com>
2020-09-26 20:04:47 +0300

    variable

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "hcloud_token" {
+  default = "u6VtWcRBzEv1gJHIOQ7gZ9OI1vWDtQlvGj8uLV46l76FeA92YQ317VJEC4ou8JUV"
+}
+variable "my_ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2g1JJtRylAPIee6PJAPBnaF070h9PS7kjJ5vcU8bb1qQa6u3sgAA46riJlNRRMkKI2fXlqZFquqtDniRTxzCaGzbJw3nve1HB9c6XOyvbrR2yjDjFR02SQncD1v1yPqkLcUh93cQNxAhoDPVbmq1wgqcAzJR2wE1/dC7PDVvkpy+jbcQzMj2gfR6tGDb7YJD3XpgbH6sT80FFDlZYgsGaUmumYA0wHW61aRDyzYpRVayYeGsQbRoEV6kOGikhc4gijS5hfU/Ckbw+O7QqSAON18eTgChTLrpqNxb/FDxuV5rKUOMPgWjMrzyqwX3/0XBEhjNDX9zvk/3XuqTfn6qN"
+}
+variable "access_key" {
+  default = "AKIAZMJMH6DA3THRGMQC"
+  type    = string
+}
+variable "secret_key" {
+  default = "uSwMqSZtjhJYY0Ypvr3nhxmNn9lSGa+mRiq5+nW1"
+  type    = string
+}
+variable "aws_region" {
+  default = "us-east-2"
+}
